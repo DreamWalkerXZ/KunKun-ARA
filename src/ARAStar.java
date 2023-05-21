c80ddef34437d841e4a3fea081db359055f39121
DreamWalkerXZ <dreamwalkerxz@outlook.com>
2023-05-21 20:36:45 +0800

    更改了ARAStar算法的调用形式

--- src/ARAStar.java
+++ src/ARAStar.java
@@ -3,18 +3,24 @@ import edu.princeton.cs.algs4.Stack;
 import java.util.ArrayList;
 
 public class ARAStar {
+    private int[][] maze;
+    private Node start;
+    private Node goal;
     private static final int[][] DIRECTIONS = { { -1, 0 }, { 1, 0 }, { 0, -1 }, { 0, 1 } };
-    static MinPQ<Node> OPEN = new MinPQ<>();
-    static ArrayList<Node> CLOSED = new ArrayList<>();
-    static MinPQ<Node> INCONS = new MinPQ<>();
-    static int height;
-    static int width;
-    static double epsilon;
-    static double epsilonPrime;
-    static double lastEpsilonPrime;
-    static Node[][] Accessed;
+    private MinPQ<Node> OPEN = new MinPQ<>();
+    private ArrayList<Node> CLOSED = new ArrayList<>();
+    private MinPQ<Node> INCONS = new MinPQ<>();
+    private int height;
+    private int width;
+    private double epsilon;
+    private double epsilonPrime;
+    private double epsilonDecreaseFactor;
+    private final double minEpsilon = 1.0;
+    private Node[][] accessed;
+    private Stack<Node> path;
+    private boolean isFirstIteration = true;
 
-    public static void improvePath(int[][] maze, Node goal, Stack<Node> path){
+    public void improvePath(int[][] maze, Node goal, Stack<Node> path){
         Node s;
         while((!OPEN.isEmpty()) && ((s = findMinF(OPEN,fValue(goal))) != null)) {
             CLOSED.add(s);
@@ -35,11 +41,11 @@ public class ARAStar {
                 int newRow = s.row + direction[0];
                 int newCol = s.col + direction[1];
                 if (newRow >= 0 && newRow < height && newCol >= 0 && newCol < width && maze[newRow][newCol] == 0){
-                    if (Accessed[newRow][newCol] == null){
-                        Accessed[newRow][newCol] = new Node(newRow,newCol,Double.POSITIVE_INFINITY,
+                    if (accessed[newRow][newCol] == null){
+                        accessed[newRow][newCol] = new Node(newRow,newCol,Double.POSITIVE_INFINITY,
                                 heuristic(new int[]{newRow,newCol},new int[]{height-1,width-1}), s);
                     }
-                    Node t = Accessed[newRow][newCol];
+                    Node t = accessed[newRow][newCol];
                     if (t.parent != null && t.parent.g > s.g) t.parent = s;
                     if (t.g > s.g + heuristic(new int[]{s.row,s.col},new int[]{t.row,t.col})){
                         t.g = s.g + heuristic(new int[]{s.row,s.col},new int[]{t.row,t.col});
@@ -53,7 +59,7 @@ public class ARAStar {
         }
     }
 
-    public static double findPath(int[][] maze, int[] start, int[] goal, double epsilon, Stack<Node> path) {
+    public double findPath(int[][] maze, int[] start, int[] goal, double epsilon, Stack<Node> path) {
         int numRows = maze.length;
         int numCols = maze[0].length;
 
@@ -100,11 +106,11 @@ public class ARAStar {
         return row >= 0 && row < numRows && col >= 0 && col < numCols;
     }
 
-    private static double fValue(Node s){
+    private double fValue(Node s){
         return s.g + epsilonPrime * s.h;
     }
 
-    private static Node findMinF(MinPQ<Node> open, double fGoal){
+    private Node findMinF(MinPQ<Node> open, double fGoal){
         if (!open.isEmpty()) {
             Node node = open.min();
             for (Node node1 : open) {
@@ -129,7 +135,7 @@ public class ARAStar {
         else return null;
     }
 
-    public static double findMinSum(){
+    public double findMinSum(){
         if (!OPEN.isEmpty() && !INCONS.isEmpty()) {
             return Math.min(OPEN.min().g + OPEN.min().h, INCONS.min().g + INCONS.min().h);
         }
@@ -142,19 +148,17 @@ public class ARAStar {
         else return 0.01;
     }
 
-    public static void publish(int[][] maze, Stack<Node> path){
+    public void publish(int[][] maze, Stack<Node> path){
         int[][] mazeCopy = new int[maze.length][maze[0].length];
         for (int i = 0; i < maze.length; i++) {
             for (int j = 0; j < maze[0].length; j++) {
                 mazeCopy[i][j] = maze[i][j];
             }
         }
-        System.out.println("Path with epsilonPrime = " + lastEpsilonPrime + ", length = " + path.size() + ":");
+        System.out.println("Path with epsilonPrime = " + epsilonPrime + ", length = " + path.size() + ":");
         for (Node node : path) {
-            // System.out.println("(" + node.row + ", " + node.col + ")");
             mazeCopy[node.row][node.col] = 2;
         }
-        // Print the maze with the path
         for (int[] ints : mazeCopy) {
             for (int j = 0; j < mazeCopy[0].length; j++) {
                 if (ints[j] == 0) {
@@ -170,52 +174,50 @@ public class ARAStar {
         System.out.println();
     }
 
-    public static void main(String[] args) {
-        int[][] maze = {
-                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
-                {0,1,0,0,1,0,0,0,1,1,0,1,0,1,0},
-                {1,1,0,1,0,0,0,0,0,0,1,0,1,1,0},
-                {1,0,0,1,0,0,1,1,1,0,1,0,0,1,0},
-                {0,0,0,0,0,1,1,0,0,0,1,0,1,1,0},
-                {1,0,1,1,0,0,1,0,1,0,0,0,0,0,0},
-                {0,0,0,1,0,1,1,0,1,1,0,1,1,1,1},
-                {1,0,0,1,1,0,0,0,0,0,0,0,0,1,0},
-                {1,1,1,1,0,0,1,1,1,1,0,1,1,1,0},
-                {1,0,0,0,0,1,0,0,0,1,1,0,0,1,0},
-                {1,0,1,1,1,0,0,1,0,0,1,0,0,0,0},
-                {0,0,0,0,0,0,1,1,0,0,1,0,1,1,0},
-                {1,0,1,0,1,1,0,0,0,0,1,0,1,1,0},
-                {1,0,0,0,1,0,0,1,1,1,1,0,1,1,0},
-                {1,1,0,1,1,1,0,0,0,0,0,0,1,1,0}
-        };
+    public Stack<Node> firstIterate() {
+        accessed[0][0] = start;
+        OPEN.insert(start);
+        improvePath(maze, goal, path);
+        epsilonPrime = Math.min(epsilon, goal.g / findMinSum());
+        publish(maze, path);
+        return path;
+    }
 
-        height = maze.length;
-        width = maze[0].length;
-        Accessed = new Node[height][width];
-        epsilon = 5;
-        epsilonPrime = epsilon;
-        double epsilonDecreaseFactor = 0.9;
-        double minEpsilon = 1.0;
-        Node start1 = new Node(0,0,0,height + width - 2,null);
-        Node goal1 = new Node(height - 1,width - 1,Double.POSITIVE_INFINITY,0,null);
-        Accessed[0][0] = start1;
-        OPEN.insert(start1);
-        Stack<Node> path = new Stack<>();
-        improvePath(maze,goal1,path);
-        epsilonPrime = Math.min(epsilon, goal1.g/findMinSum());
-        lastEpsilonPrime = epsilonPrime;
-        publish(maze,path);
-
-        while (epsilonPrime >= minEpsilon) {
+    public Stack<Node> otherIterate() {
+        if (epsilonPrime >= minEpsilon) {
             epsilon *= epsilonDecreaseFactor;
-            while(!INCONS.isEmpty())
-            OPEN.insert(INCONS.delMin());
+            while (!INCONS.isEmpty())
+                OPEN.insert(INCONS.delMin());
             CLOSED.clear();
-            improvePath(maze,goal1,path);
-            epsilonPrime = Math.min(epsilon, goal1.g/findMinSum());
-            publish(maze,path);
-            if (lastEpsilonPrime == 1) break;
-            lastEpsilonPrime = epsilonPrime;
+            improvePath(maze, goal, path);
+            epsilonPrime = Math.min(epsilon, goal.g / findMinSum());
+            publish(maze, path);
+            return path;
+        } else {
+            System.out.println("epsilonPrime <= 1!");
+            return null;
         }
+    }
+
+    public Stack<Node> iterate() {
+        if (isFirstIteration) {
+            isFirstIteration = false;
+            return firstIterate();
+        } else {
+            return otherIterate();
+        }
+    }
+
+    public ARAStar(int[][] maze, double epsilon, double epsilonDecreaseFactor) {
+        this.maze = maze;
+        this.height = maze.length;
+        this.width = maze[0].length;
+        this.accessed = new Node[height][width];
+        this.epsilon = epsilon;
+        this.epsilonPrime = epsilon;
+        this.start = new Node(0, 0, 0, height + width - 2, null);
+        this.goal = new Node(height - 1, width - 1, Double.POSITIVE_INFINITY, 0, null);
+        this.path = new Stack<>();
+        this.epsilonDecreaseFactor = epsilonDecreaseFactor;
     }
 }
--- src/App.java
+++ src/App.java
@@ -3,55 +3,34 @@ import edu.princeton.cs.algs4.Stack;
 public class App {
     public static void main(String[] args) {
         int[][] maze = {
-                {0, 0, 0, 0, 1, 1, 1, 1, 0, 0},
-                {0, 0, 1, 0, 0, 0, 0, 1, 0, 0},
-                {1, 1, 1, 0, 0, 0, 0, 1, 0, 1},
-                {0, 0, 0, 0, 1, 1, 1, 1, 0, 0},
-                {0, 1, 1, 1, 1, 0, 0, 0, 0, 0},
-                {0, 0, 0, 0, 1, 0, 0, 1, 1, 0},
-                {1, 0, 0, 0, 0, 0, 1, 1, 0, 0},
-                {0, 0, 1, 1, 1, 1, 0, 0, 0, 1},
-                {1, 0, 1, 0, 0, 0, 1, 0, 0, 0},
-                {1, 0, 0, 0, 1, 0, 0, 0, 0, 0}
+                {0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {0,1,0,0,1,0,0,0,1,1,0,1,0,1,0},
+                {1,1,0,1,0,0,0,0,0,0,1,0,1,1,0},
+                {1,0,0,1,0,0,1,1,1,0,1,0,0,1,0},
+                {0,0,0,0,0,1,1,0,0,0,1,0,1,1,0},
+                {1,0,1,1,0,0,1,0,1,0,0,0,0,0,0},
+                {0,0,0,1,0,1,1,0,1,1,0,1,1,1,1},
+                {1,0,0,1,1,0,0,0,0,0,0,0,0,1,0},
+                {1,1,1,1,0,0,1,1,1,1,0,1,1,1,0},
+                {1,0,0,0,0,1,0,0,0,1,1,0,0,1,0},
+                {1,0,1,1,1,0,0,1,0,0,1,0,0,0,0},
+                {0,0,0,0,0,0,1,1,0,0,1,0,1,1,0},
+                {1,0,1,0,1,1,0,0,0,0,1,0,1,1,0},
+                {1,0,0,0,1,0,0,1,1,1,1,0,1,1,0},
+                {1,1,0,1,1,1,0,0,0,0,0,0,1,1,0}
         };
-        int[] start = { 0, 0 };
-        int[] goal = { maze.length - 1, maze[0].length - 1 };
-        double epsilon = 5.0;
-        double epsilonDecreaseFactor = 0.9;
-        double minEpsilon = 1.0;
-        Stack<Node> path = new Stack<>();
 
-        while (epsilon >= minEpsilon) {
-            epsilon = ARAStar.findPath(maze, start, goal, epsilon, path);
-            int[][] mazeCopy = new int[maze.length][maze[0].length];
-            for (int i = 0; i < maze.length; i++) {
-                for (int j = 0; j < maze[0].length; j++) {
-                    mazeCopy[i][j] = maze[i][j];
-                }
+        ARAStar araStar = new ARAStar(maze, 5, 0.9);
+        while (true) {
+            Stack<Node> path = araStar.iterate();
+            if (path != null) {
+                // for (Node node : path) {
+                //     System.out.print(node.row + " " + node.col + " ");
+                // }
+                // System.out.println();
+            } else {
+                break;
             }
-
-            System.out.println("Path with epsilon = " + epsilon + ", length = " + path.size() + ":");
-            for (Node node : path) {
-                // System.out.println("(" + node.row + ", " + node.col + ")");
-                mazeCopy[node.row][node.col] = 2;
-            }
-
-            // Print the maze with the path
-            for (int i = 0; i < mazeCopy.length; i++) {
-                for (int j = 0; j < mazeCopy[0].length; j++) {
-                    if (mazeCopy[i][j] == 0) {
-                        System.out.print("  ");
-                    } else if (mazeCopy[i][j] == 1) {
-                        System.out.print("██");
-                    } else if (mazeCopy[i][j] == 2) {
-                        System.out.print("░░");
-                    }
-                }
-                System.out.println();
-            }
-            System.out.println();
-
-            epsilon *= epsilonDecreaseFactor;
         }
     }
 }
